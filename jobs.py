c32037f097b5a484f74cbbaf401a745705abf6c2
Shun Liang <shun.liang.public@gmail.com>
2016-01-23 23:27:22 +0000

    First working version of job scraping. Single threaded.

--- jobs.py
+++ jobs.py
@@ -5,37 +5,63 @@ import re
 import requests
 
 with open('skills.json') as skills_file:
-    programming_languages = json.load(skills_file)['languages']
-    programming_languages_dict = {lang.lower(): [] for lang in
-            programming_languages}
+    skills_json = json.load(skills_file)
+    single_word_programming_languages = skills_json['single_word_languages']
+    single_word_programming_languages_lower = [lang.lower() for lang in
+            single_word_programming_languages]
+    multiple_word_programming_languages = skills_json['multiple_word_languages']
+    multiple_word_programming_languages_lower = [lang.lower() for lang in
+            multiple_word_programming_languages]
+    programming_languages_dict = {lang: [] for lang in
+            single_word_programming_languages_lower +
+            multiple_word_programming_languages_lower}
 print(programming_languages_dict)
 
-split_pattern = r'[\w\'\|\-+]+'
-#tag_pattern = '<[^>*]>'
+split_pattern = r'[\w\'\|\-\+#&]+'
 url_pattern = 'http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*\(\),]|(?:%[0-9a-fA-F][0-9a-fA-F]))+'
 email_pattern = '[\w.-]+@[\w.-]+'
 
 root_post_request = requests.get('https://hacker-news.firebaseio.com/v0/item/10822019.json')
 job_post_ids = root_post_request.json()['kids']
 
-for job_post_id in job_post_ids:
+def scrape_jobs():
+    for job_post_id in job_post_ids:
+        job_post_text = _get_job_post_text(job_post_id)
+        if job_post_text:
+            job_post_text = BeautifulSoup(job_post_text, 'html.parser').get_text(separator = ' ').lower()
+            job_post_text = re.sub('%s|%s' % (url_pattern, email_pattern), '', job_post_text)
+            words = re.findall(split_pattern, job_post_text)
+            single_word_programming_languages_set = set({})
+            multiple_word_programming_languages_set = set({})
+            for word in words:
+                if word in single_word_programming_languages_lower:
+                    single_word_programming_languages_set.add(word)
+            for lang in multiple_word_programming_languages_lower:
+                if lang in job_post_text:
+                    multiple_word_programming_languages_set.add(lang)
+            for lang in single_word_programming_languages_set |\
+                multiple_word_programming_languages_set:
+                programming_languages_dict[lang].append(job_post_id)
+
+    print(programming_languages_dict)
+    print({lang: len(programming_languages_dict[lang]) for lang in programming_languages_dict})
+
+def show_jobs(job_post_id_list):
+    print(job_post_json['text'])
+
+def _get_job_post_text(job_post_id):
     job_post_request = requests.get('https://hacker-news.firebaseio.com/v0/item/%s.json' %
             job_post_id)
     if job_post_request.status_code == 200:
         job_post_json = job_post_request.json()
         if 'text' in job_post_json:
-            job_post_text = BeautifulSoup(job_post_json['text'], 'html.parser').get_text(separator = ' ').lower()
-            job_post_text = re.sub('%s|%s' % (url_pattern, email_pattern), '', job_post_text)
-            words = re.findall(split_pattern, job_post_text)
-            print(job_post_text)
-            print(words)
-            for word in words:
-                if word in programming_languages_dict:
-                    programming_languages_dict[word].append(job_post_id)
+            return job_post_json['text']
         else:
             print(job_post_request.text)
+            return None
     else:
-        print('Can\t retrieve job post %s' % job_post_id)
+        print('Can\t retrive job post %s, HTTP response code: %s' % (job_post_id, job_post_request_status_code))
+        return None
 
-print(programming_languages_dict)
-print({lang: len(programming_languages_dict[lang]) for lang in programming_languages_dict})
+if __name__ == '__main__':
+    scrape_jobs()
